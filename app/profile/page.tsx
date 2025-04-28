5a7396aac67f73bd3f572231cd151086a57a0e34
Ashley-Nicole18 <ashleynicole.gellado-23@cpu.edu.ph>
2025-04-29 00:52:46 +0800

    Updating

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
+import Image from 'next/image';
 
 const ProfileTutee = () => {
   return (
     <div>
-      
       <h2>Welcome to the Tutee Profile</h2>
       <p>This is the profile page for the Tutee.</p>
       
-      
-      <img
-        src="/img/Bg-Tutee.jpg" 
+      <Image
+        src="/img/Bg-Tutee.jpg" // Your image path
         alt="Tutee Profile"
-        width={200}
-        height={200}
+        width={200} // Desired width
+        height={200} // Desired height
+        priority // Optionally add `priority` for images that should load quickly
       />
     </div>
   );
