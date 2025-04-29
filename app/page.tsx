d964350609f0ce34a38cb63d47fc49c986a32232
Ashley-Nicole18 <ashleynicole.gellado-23@cpu.edu.ph>
2025-04-29 16:08:04 +0800

    Converting hover sidebar to static

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from "next/image";
-import HoverSidebar from "../components/HoverSidebar";
+import HoverSidebar from "../components/StaticSidebar";
 
 const page = () => {
   return (
