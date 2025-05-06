8889f9c59304065b201ba64ecb2d45cad42ed826
yuki-ni <atheenaashley.pineda-22@cpu.edu.ph>
2025-05-06 16:53:46 +0800

    updated tutee profile

--- app/profile-tutee/page.tsx
+++ app/profile-tutee/page.tsx
@@ -1,15 +1,14 @@
 
 import ProfileTutee from "@/components/profile/ProfileTutee";
-import TuteeProfileNav from "@/components/TuteeProfileNav"
+
 
 export default function Home() {
   return (
-    <div className="min-h-screen pt-10 mr-1">
-      <TuteeProfileNav/>
+    
     <div className="flex items-center justify-center min-h-screen">
       <ProfileTutee />
     </div>
-    </div>
+    
 
   );
 }
--- app/review-form/page.tsx
+++ app/review-form/page.tsx
@@ -1,13 +1,13 @@
 
 import ReviewForm from '@/components/profile/TuteeReviewForm'
 import React from 'react'
-import TuteeProfileNav from "@/components/TuteeProfileNav"
+
 import Sidebar from '@/components/TuteeSidebar'
 
 const page = () => {
   return (
     <div className="min-h-screen pt-10 mr-1 ">
-      <TuteeProfileNav/>
+      
       <Sidebar/>
       <div className="flex items-center justify-center min-h-screen">
         <ReviewForm />
